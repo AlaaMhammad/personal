1df1571f299bfd992a071905bffe2da87910c995
ALaa Mhammad <alaamhammad2003@gmail.com>
2024-08-24 11:42:54 +0300

    new updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index'])->name('index');
+Route::get('/resume', [MainController::class, 'resume'])->name('resume');
+Route::get('/projects', [MainController::class, 'projects'])->name('projects');
+Route::get('/contact', [MainController::class, 'contact'])->name('contact');
+Route::post('/contact', [MainController::class, 'contact_data'])->name('contact_data');
+
+
+
+
+
